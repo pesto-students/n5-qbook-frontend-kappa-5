3f6b957c7b9d20f730052e8f4f062a0608ed3ad4
parixit.rao <mohit.niwadunge@gmail.com>
2021-08-15 19:37:50 +0530

    TAS-41: confirmation page changes up

--- pages/booking.js
+++ pages/booking.js
@@ -1,4 +1,4 @@
-import React,{useState,useRef} from "react";
+import React,{useState,useRef, useEffect} from "react";
 import firebase from '../firebase'
 import LoginLayout from "layouts/LoginLayout.js";
 import config from "../config/config";
@@ -26,28 +26,28 @@ export default function Booking() {
   const [deviceId, setDeviceId] =  useState('');
   const router = useRouter();
   const { uuid } = router.query;
-        
-  // const getdeviceId = () => {
-  //   var uniqueId = getUA
-  //  //var uniqueId = MediaDeviceInfo.deviceId;
-  //  //var uniqueId = getUA();
-  //   console.log(uniqueId)
-  //   setDeviceId(uniqueId);
-  // };
-
-    const [phoneNumber,setPhoneNumber] = useState('');
-    const [fullName,setFullName] = useState('');
-    const [otp,setOtp] = useState('');
-    const capthaRef = useRef();
-    const configureCaptha = () =>{    
+  const [phoneNumber,setPhoneNumber] = useState('');
+  const [errorMessage,setErrorMessage] = useState('');
+  const [paymnetMode,setPaymnetMode] = useState('online');
+  const [fullName,setFullName] = useState('');
+  const [checkAvailable,setCheckAvailable] = useState('');
+  const [isCheckAvailable,setIsCheckAvailable] = useState('');
+  const [otp,setOtp] = useState('');
+  const capthaRef = useRef();
+  const configureCaptha = () =>{    
       if(window.recaptchaVerifier) {       
         capthaRef.current.innerHTML = `<div id="sign-in-button" ></div>`;
-    }    
+    }
+    setIsCheckAvailable(true);   
     window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
       'size': 'invisible',
       'callback': (response) => {        
       }
-    })}
+    })
+  }
+  const getSetPaymnetMode = (e) =>{
+    setPaymnetMode(e.target.value);
+  }
     const signInPatient = () =>{
       configureCaptha();    
       const appVerifier = window.recaptchaVerifier;    
@@ -60,90 +60,113 @@ export default function Booking() {
           });
     }
     const onSubmitOtp =() =>{
-
-      // const code = otp;
-      // window.confirmationResult.confirm(code).then((result) => {
-      //   const user = result.user;
-      //   console.log(JSON.stringify(user));
-      // }).catch((error) => {
-      //   console.log("otp invalid",error.message);
-      //   window.recaptchaVerifier.reset();
-      //   window.recaptchaVerifier.clear();
-      // });
-
-      displayRazorpay();
+        const code = otp;
+        window.confirmationResult.confirm(code).then((result) => {
+          const user = result.user;
+          console.log(JSON.stringify(user));
+        }).catch((error) => {
+          setErrorMessage('otp invalid');
+          console.log("otp invalid",error.message);
+          window.recaptchaVerifier.reset();
+          window.recaptchaVerifier.clear();
+        });
     }
-
-      let displayRazorpay = async () => {
-        const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js')
-        const webtoken = await firebaseCloudMessaging.init();
-        if (!res) {
-          alert('Razorpay SDK failed to load. Are you online?')
-          return
-        }
-    
-        const dataQrCode = await fetch(config.BASE_API_URL+'/booking/checkAvailability?uuid='+uuid, { method: 'GET' }).then((t) =>
+    const onBookAppoinment =() =>{
+      if(fullName && phoneNumber && otp){
+        displayRazorpay();
+      }else{
+        setErrorMessage('please fill all required Fields !');
+      }
+    }
+    let checkAvailability= async () =>{
+      const dataQrCode = await fetch(config.BASE_API_URL+'/booking/checkAvailability?uuid='+uuid, { method: 'GET' }).then((t) =>
           t.json()
-        )
-        // const dataQrCode = await getAsyncData('/booking/checkAvailability?uuid='+uuid,{uuid});
-        // const data = await fetch('http://localhost:1337user/generate-code', { method: 'GET' }).then((t) =>
-        //   t.json()
-        // )
+        );
+        console.log('dataQrCode',dataQrCode, uuid);
+        setCheckAvailable(dataQrCode);
+    }
+    let displayRazorpay = async () => {
+      const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js')
+      const webtoken = await firebaseCloudMessaging.init();
+      if (!res) {
+        alert('Razorpay SDK failed to load. Are you online?')
+        return
+      }
+      console.log('checkAvailable', checkAvailable, webtoken);
+      if(checkAvailable && checkAvailable.data){
         
-        console.log('dataQrCode', dataQrCode, webtoken);
-        if(dataQrCode && dataQrCode.data){
-          
-            const options = {
-              key: 'rzp_test_Mj02y5458xshqx',
-              currency: 'INR',
-              amount: dataQrCode.data.fee || 500,//data.amount.toString(),
-              order_id: dataQrCode.data.orderId,
-              name: 'Doctor Payment',
-              description: 'Please go ahead for payemnt !!',
-              image: 'https://www.newzealand.com/assets/Operator-Database/f59158f2b6/img-1536060335-6557-12242-p-6F1EB578-C4BC-BE00-91796DF7718B39A2-2544003__aWxvdmVrZWxseQo_CropResizeWzk0MCw1MzAsNzUsImpwZyJd.jpg',
-              handler: function (response) {
-                // alert(response.razorpay_payment_id)
-                // alert(response.razorpay_order_id)
-                // alert(response.razorpay_signature)
-                console.log('response',response);
-                let createData ={
-                  "name": fullName,
-                  "mobileNum": phoneNumber,
-                  "isMobileNumVerified": true,
-                  "paymentMode": "online",
-                  "uuid": uuid,
-                  "razorpay_order_id": response.razorpay_order_id,
-                  "razorpay_payment_id": response.razorpay_payment_id,
-                  "razorpay_signature": response.razorpay_signature,
-                  "token":webtoken || ''
-                }
-                bookingCreateApi(createData);
-              },
-              prefill: {
-                phone_number: phoneNumber
+          const options = {
+            key: 'rzp_test_Mj02y5458xshqx',
+            currency: 'INR',
+            amount: checkAvailable.data.fee || 500,//data.amount.toString(),
+            order_id: checkAvailable.data.orderId,
+            name: 'Doctor Payment',
+            description: 'Please go ahead for payemnt !!',
+            image: 'https://www.newzealand.com/assets/Operator-Database/f59158f2b6/img-1536060335-6557-12242-p-6F1EB578-C4BC-BE00-91796DF7718B39A2-2544003__aWxvdmVrZWxseQo_CropResizeWzk0MCw1MzAsNzUsImpwZyJd.jpg',
+            handler: function (response) {
+              // alert(response.razorpay_payment_id)
+              // alert(response.razorpay_order_id)
+              // alert(response.razorpay_signature)
+              console.log('response',response);
+              let createData ={
+                "name": fullName,
+                "mobileNum": phoneNumber,
+                "isMobileNumVerified": otp?true:false,
+                "paymentMode": paymnetMode,
+                "uuid": uuid,
+                "order_id": checkAvailable.data.orderId,
+                "razorpay_order_id": response.razorpay_order_id,
+                "razorpay_payment_id": response.razorpay_payment_id,
+                "razorpay_signature": response.razorpay_signature,
+                "token":webtoken || ''
               }
+              bookingCreateApi(createData);
+            },
+            prefill: {
+              phone_number: phoneNumber
             }
-            const paymentObject = new window.Razorpay(options)
-            paymentObject.open()
-        }
-       
+          }
+          const paymentObject = new window.Razorpay(options)
+          paymentObject.open()
       }
-      const bookingCreateApi = async(data) =>{
-        const bookingResponse = await getAsyncPostData('/booking/create',data); 
-        console.log('bookingResponse',bookingResponse);
+      
+    }
+    const bookingCreateApi = async(data) =>{
+      const bookingResponse = await getAsyncPostData('/booking/create',data); 
+      if(bookingResponse && bookingResponse.data && bookingResponse.data.searchToken){
+        router.push('/confirmation?searchToken'+bookingResponse.data.searchToken)
+      }else{
+        setErrorMessage('Some issue in Book an appoinmnet please try again !');
       }
+      console.log('bookingResponse',bookingResponse);
+    }
+    useEffect(() => {
+      if(uuid){
+        checkAvailability();
+      }
+    },[uuid]);
   return (
     <>
       <div className="container mx-auto px-4 h-full">
         <div className="flex content-center items-center justify-center h-full">
           <div className="w-full lg:w-6/12 px-4">
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
-              <div className="rounded-t mb-0 px-6 py-6">
+            {checkAvailable && checkAvailable.data && checkAvailable.data.orderId? <div className="rounded-t mb-0 px-6 py-6">
                 <div className="text-center mb-3">
                   <h6 className="text-blueGray-500 text-sm font-bold">Book an Appointment</h6>
-                </div>             
+                </div>  
+                {errorMessage?<div className="text-center mb-3">
+                  <h6 className="text-red-500 text-sm font-bold">! Error : {errorMessage}</h6>
+                </div>:''}           
                 <hr className="mt-6 border-b-1 border-blueGray-300" />
+              </div>:
+                <div className="rounded-t mb-0 px-6 py-6">
+                <div className="text-center mb-3">
+                  <h6 className="text-blueGray-500 text-sm font-bold">Book Appointment is not available for now !!. </h6>
+                </div>
               </div>
+                }
+              {checkAvailable && checkAvailable.data && checkAvailable.data.orderId? 
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">               
                 <form>
                 <div ref={capthaRef}><div id="sign-in-button" ></div></div>
@@ -180,15 +203,28 @@ export default function Booking() {
                   <div className="text-center mt-6">
                     <button
                       className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                      type="button"  onClick={onSubmitOtp}>Verify OTP & Book Appointment</button>
+                      type="button"  onClick={onSubmitOtp}>Verify OTP</button>
+                  </div>
+                  <div className="mt-6">
+                    <span className="text-gray-700">Payment Mode</span>
+                    <div className="mt-2">
+                      <label className="inline-flex items-center px-2 py-2 block uppercase text-blueGray-600 text-xs font-bold mb-2">
+                        <input type="radio" className="form-radio" name="accountType" value="cash" checked={paymnetMode === 'cash'?true:false} onChange={getSetPaymnetMode} />
+                        <span className="ml-2">Cash</span>
+                      </label>
+                      <label className="inline-flex items-center px-2 py-2 block uppercase text-blueGray-600 text-xs font-bold mb-2">
+                        <input type="radio" className="form-radio" name="accountType" value="online" checked={paymnetMode === 'online'?true:false}  onChange={getSetPaymnetMode} />
+                        <span className="ml-2">Online</span>
+                      </label>
+                    </div>
                   </div>
                   <div className="text-center mt-6">
-                    {/* <button
-                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                      type="button"  onClick={getdeviceId}>generate unique id</button> */}
+                    <button
+                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150 "
+                      type="button"  onClick={onBookAppoinment}>Book Appointment</button>
                   </div>
                 </form>
-              </div>
+              </div>:''}
             </div>
           </div>
         </div>
