46632560fe4b0e2e6e9d948f2c0a1c0520c2c3eb
Gaurav Tayal <gauravtayal2013@gmail.com>
2021-08-15 22:15:14 +0530

    add QRCode

--- components/Cards/CardQrCode.js
+++ components/Cards/CardQrCode.js
@@ -2,18 +2,32 @@ import React,{useState,useEffect} from "react";
 import Toggle from 'components/Sidebar/Toggle';
 import { updateConfig } from '../../slices/settingsSlice'
 import { useDispatch } from 'react-redux';
+import QRCode from 'qrcode.react';
 import {getAsyncPostData,getAsyncData} from '../../utils/ApiRequests';
-
+import QrCode from "pages/doctor/qrcode";
+const MINUTE_MS = 300000;
 export default function CardQrCode() {
-    const loginInfo = JSON.parse(sessionStorage.getItem("doctor_login"));
+
+    const [url,seturl] = useState('');
+    const [Index,setIndex] = useState(0);
+    //const loginInfo = JSON.parse(sessionStorage.getItem("doctor_login"));
 const getQrCode = async() =>{
     const response = await getAsyncData('/user/generate-code'); 
-    debugger;
-    console.log(response,"response in QR")
+    seturl(response.data);
+    //console.log(response,"response in QR")
   } 
   
 useEffect( () => {
+  if(Index == 0){
     getQrCode();
+  }
+  const interval = setInterval(() => {
+    getQrCode();
+    let i = Index;
+    setIndex(i++);
+  }, MINUTE_MS);
+
+  return () => clearInterval(interval); 
 }, [])
 
  
@@ -21,15 +35,18 @@ useEffect( () => {
   return (
     <>
       <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16">
+      <div className="rounded-t bg-white mb-0 px-6 py-6">
+          <div className="text-center flex justify-between">
+            <h6 className="text-blueGray-700 text-xl font-bold">Scan for Booking </h6> 
+          </div>
+        </div>
         <div className="px-6">
           <div className="flex flex-wrap justify-center">
             <div className="w-full px-4 flex justify-center">
               <div className="relative">
-                <img
-                  alt="..."
-                  src={loginInfo?.result?.image}
-                  className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
-                />
+                {url && url !== "" &&
+                <QRCode value={url} includeMargin={true} level="Q" size="150"/>
+               }
               </div>
             </div>
           </div>
