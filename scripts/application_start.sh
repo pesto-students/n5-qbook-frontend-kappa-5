bb1a993afbe31e39fbd7b5c8d4e1362bcc504f1d
parixit.rao <mohit.niwadunge@gmail.com>
2021-08-17 14:59:40 +0530

    script added

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,11 +11,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-#install node modules
-npm install
-
-#create node build
-npm run build
+#install node modules & create node build
+npm install && npm run build
 
 #start our node app in the background
 npm run start > app.out.log 2> app.err.log < /dev/null &  
