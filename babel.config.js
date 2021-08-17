b0a99c40374ded1fa25772f478ec970a7e4bb8ee
Gaurav Tayal <gauravtayal2013@gmail.com>
2021-08-17 11:37:16 +0530

    add code

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// todo: https://github.com/babel/babel/issues/8529 :'(
-module.exports = {
-    presets: ['@babel/preset-env', ['@babel/preset-react', {
-        "runtime": "automatic"
-    }]],
-};
