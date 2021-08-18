27071521038a5bf39b98ce28b6fb46d75c045e0d
Gaurav Tayal <gauravtayal2013@gmail.com>
2021-08-18 18:54:47 +0530

    add code

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/auth/doctorlogin":["static\u002Fchunks\u002Fpages\u002Fauth\u002Fdoctorlogin.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fauth\u002Fdoctorlogin"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
